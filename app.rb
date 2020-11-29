9d8e812676fe5829d017908fa4fd73b6a93e665f
Nick Nobile <nickcnobile@gmail.com>
2020-11-29 17:04:45 -0500

    commit tree 323d55334cd0158b40a64467010e894bddbd12e4

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Nick Nobile"
+     end
+
+     get '/hometown' do
+        "My hometown is Melville, New York"
+     end
+
+     get '/favorite-song' do
+        "My favorite song is 'Money Trees' by Kendrick Lamar"
+     end
+
+
 end
